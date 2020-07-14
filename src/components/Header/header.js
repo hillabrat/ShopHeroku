ecfd28bfc6fd9c3b6e256a4a881de6587f2e37a6
hillabrat <hillabrat@gmail.com>
2020-07-14 10:44:17 +0300

    change classes to functions + useState

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./header.css";
 
-function Header() {
-  return (
-    <div className="Header">
-      <h1>Products</h1>
-    </div>
-  );
-}
+const Header = () => (
+  <div className="Header">
+    <h1>Products</h1>
+  </div>
+);
 
 export default Header;
--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,35 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./product.css";
 
-class Product extends Component {
-  state = {
-    productImg: this.props.image,
-    productTitle: this.props.title,
-    productQuantity: this.props.quantity,
-    updateCart: this.props.updateCart,
-  };
-  render() {
-    return (
-      <div className="product">
-        <h2>{this.state.productTitle}</h2>
-        <img src={this.state.productImg}></img>
-        <div className="quantity">quantity: {this.state.productQuantity}</div>
-        <button
-          onClick={() => {
-            if (this.state.productQuantity > 0) {
-              this.setState({
-                productQuantity: this.state.productQuantity - 1,
-              });
-              this.state.updateCart();
-            } else alert("product is out of stock");
-          }}
-        >
-          Add to Cart
-        </button>
-      </div>
-    );
-  }
-}
+const Product = (props) => {
+  const [productQuantity, setProductQuantity] = useState(props.quantity);
+
+  return (
+    <div className="product">
+      <h2>{props.title}</h2>
+      <img src={props.image}></img>
+      <div className="quantity">quantity: {productQuantity}</div>
+      <button
+        onClick={() => {
+          if (productQuantity > 0) {
+            setProductQuantity(productQuantity - 1);
+            //this.state.updateCart();
+          } else alert("product is out of stock");
+        }}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+};
 
 export default Product;
