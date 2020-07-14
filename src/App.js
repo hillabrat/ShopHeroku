1551e46c855b592fd487aa2bc8d5fc44756b731e
hillabrat <hillabrat@gmail.com>
2020-07-14 12:22:18 +0300

    use elad APi for products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import ladder1 from "./images/ladder1.jpg";
 import ladder2 from "./images/ladder2.jpg";
@@ -6,58 +6,32 @@ import ladder3 from "./images/ladder3.jpg";
 import "./App.css";
 import Header from "./components/Header/header";
 import Product from "./components/Product/product";
+import axios from "axios";
 
-const App = () => (
-  // state = {
-  //   itemsInCart: 0,
-  // };
+const App = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("https://quilt-flax-chemistry.glitch.me/products").then((res) => {
+      console.log(res.data);
+      setProducts(res.data);
+    });
+  }, []);
 
-  // updateCart = () => {
-  //   console.log("update Cart!!!");
-  //   this.setState({ itemsInCart: this.state.itemsInCart + 1 });
-  // };
-  <div className="App">
-    <Header />
-    <div className="Products">
-      {/* <div className="Cart">Cart Count = {this.state.itemsInCart}</div> */}
-      <Product
-        image={ladder1}
-        title="ladder 1"
-        quantity={20}
-        // updateCart={this.updateCart}
-      />
-      <Product
-        image={ladder2}
-        title="ladder 2"
-        quantity={3}
-        // updateCart={this.updateCart}
-      />
-      <Product
-        image={ladder3}
-        title="ladder 3"
-        quantity={10}
-        // updateCart={this.updateCart}
-      />
-      <Product
-        image="https://konimboimages.s3.amazonaws.com/system/photos/2654583/large/11a6f408fe8143ecb6dc7819980a6ea5.jpg"
-        title="ladder 4"
-        quantity={30}
-        // updateCart={this.updateCart}
-      />
-      <Product
-        image="https://www.intex-pool.co.il/images/itempics/316-3_18032019152431_large.jpg"
-        title="ladder 5"
-        quantity={5}
-        // updateCart={this.updateCart}
-      />
-      <Product
-        image="https://d3m9l0v76dty0.cloudfront.net/system/photos/4696013/large/4c8e6bd6e366ad91dfd1178eec7dd7ea.jpg"
-        title="ladder 6"
-        quantity={10}
-        // updateCart={this.updateCart}
-      />
+  return (
+    <div className="App">
+      <Header />
+      <div className="Products">
+        {products.map((p) => (
+          <Product
+            key={p.id}
+            title={p.title}
+            image={p.image}
+            quantity={p.quantity}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
